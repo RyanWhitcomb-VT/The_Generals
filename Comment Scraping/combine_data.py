1e42e07bd36c61f1f8ef666c7439ac4d97eed181
Ryan Whitcomb <rwhit94@vt.edu>
2016-11-30 22:25:38 -0500

    Uploaded combined data and fixed data

---
+++ Comment Scraping/combine_data.py
@@ -0,0 +1,15 @@
+# Combine processed files together
+
+import pandas as pd
+import os
+
+# Go through every csv and make it a DataFrame, then add to list
+file_list = []
+for subdir, dirs, files in os.walk('Processed_Data/'):
+    for processed_file in files[1:]:
+        file_path = "Processed_Data/" + processed_file
+        this_file = pd.read_csv(file_path)
+        file_list.append(this_file)
+        
+# Concatenate all the DateFrames together
+df = pd.concat(file_list)
